9eb81f3f060ddc190d96da871bb2aa8ec2610f99
Blackla6el <56595506+Blackla6el@users.noreply.github.com>
2022-02-08 10:12:40 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -600,5 +600,5 @@ window.onload = function() {
     loadcounter = loadcounter + 1;
   }
     var s = document.getElementsByClassName("results_count")[1]
-    s.innerText = "Results: " + loadcountertotal + "/" + meta.length + " Scroll Down To view more";	
+    s.innerText = "Results: " + loadcounter + "/" + meta.length + " Scroll Down To view more";	
 }
